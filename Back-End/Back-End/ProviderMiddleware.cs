148d554208340ce4060350114b0d6c184dbab929
NThuHang <nguyenthuhang11121999@gmail.com>
2020-10-22 15:23:12 +0700

    BackEnd

---
+++ Back-End/Back-End/ProviderMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Back_End
+{
+    public class ApiMiddleware
+    {
+        private readonly RequestDelegate next;
+        public ApiMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            context.Response.Headers.Add("Access-Control-Expose-Headers", "*");
+            await next(context);
+        }
+    }
+    public static class ProviderMiddleware
+    {
+        public static IApplicationBuilder UseApiMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ApiMiddleware>();
+        }
+    }
+}
---
+++ Back-End/DAL/Helper/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
